581b1d2f907e7727de57e1c4d617d2a6a366510a
julfikar <julfikarjim69@gmail.com>
2024-07-13 20:52:19 +0600

    update setting.py

--- weather_analysis/settings.py
+++ weather_analysis/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = get_random_secret_key()
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['disaster-hackathon-2-0.onrender.com']
 
 
 # Application definition
